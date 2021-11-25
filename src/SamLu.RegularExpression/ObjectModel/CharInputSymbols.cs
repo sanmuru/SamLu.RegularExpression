853a0b69f81009be1914ae978d63817621b9efc3
Sam Lu <549429518@qq.com>
2021-11-25 17:37:20 +0800

    1. Immigrate to .NET 6. 2. Refacting all codes.

---
+++ src/SamLu.RegularExpression/ObjectModel/CharInputSymbols.cs
@@ -0,0 +1,24 @@
+﻿using SamLu.StateMachine.ObjectModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamLu.RegularExpression.ObjectModel
+{
+    public class CharInputSymbols : InputSymbols<char>
+    {
+        public override bool HasNext(char value) => value != char.MaxValue;
+
+        public override bool HasPrevious(char value) => value != char.MinValue;
+
+        public override char Previous(char value) => (char)(value - 1);
+
+        public override char Next(char value) => (char)(value + 1);
+
+        public override bool NextTo(char x, char y) => Math.Abs(x - y) == 1;
+    }
+}
