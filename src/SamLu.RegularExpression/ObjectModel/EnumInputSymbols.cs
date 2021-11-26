143d529f3bf7826cc584f98d156ea06e3d8edb63
Sam Lu <549429518@qq.com>
2021-11-26 17:27:37 +0800

    temp20211126

---
+++ src/SamLu.RegularExpression/ObjectModel/EnumInputSymbols.cs
@@ -0,0 +1,13 @@
+﻿using SamLu.StateMachine.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamLu.RegularExpression.ObjectModel
+{
+    public class EnumInputSymbols<T> : InputSymbols<T> where T : Enum
+    {
+    }
+}
